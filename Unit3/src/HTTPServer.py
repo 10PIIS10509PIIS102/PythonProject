baaeed436241f0e4e938f73d357dd86c93995ade
subeen <subeen@subeen-virtual-machine.(none)>
2013-04-18 19:46:18 +0530

    BUG ISSUE FIXED

--- Unit3/src/HTTPServer.py
+++ Unit3/src/HTTPServer.py
@@ -1,8 +1,11 @@
 from BaseHTTPServer import BaseHTTPRequestHandler,HTTPServer
 from SimpleHTTPServer import SimpleHTTPRequestHandler
 import cgi,CGIHTTPServer
+from SocketServer import ThreadingMixIn
+import threading
+import urllib2
 
-PORT_NUMBER = 8080
+PORT_NUMBER = 8050
 
 #This class will handles any incoming request from
 #the browser 
@@ -32,7 +35,10 @@ class myHandler(SimpleHTTPRequestHandler):
 									keep_blank_values = 1) 
 
 	# Get data from fields
-		first_name = form.getvalue('place')
+		type= form.getvalue('Type')
+		state= form.getvalue('State')
+		budget= form.getvalue('Budget')
+		print type,state,budget
 		self.wfile.write(df%(form))
 		
 	def do_GET(self):
@@ -42,80 +48,48 @@ class myHandler(SimpleHTTPRequestHandler):
 		self.send_response(200)
 		self.send_header('Content-type','text/html')
 		self.end_headers()
-		form=''' <html>
-					<head>
-					<script type=text/javascript>
-					function init()
-					{
-					var dis=document.getElementsByTagName("input");
-					for(i=0;i<dis.length-1;i++)
-					dis[i].style.display="none";
-					}
-					function deinit(id)
-					{
-					if(id==4 || id==7 || id==8 || id==9)
-					{
-					for(i=1;i<=5;i++)
-					{
-					var dis=document.getElementById(id+i.toString());
-					dis.style.display="block";
-					}
-					}
-					else
-					{
-					var dis=document.getElementById(id);
-					dis.style.display="block";
-					}
-					}
-					</script>
-					</head>
-					<body bgcolor=green onload=init()>
-					<div style="font:24px monotype corsiva">
-					<b><u>Enter the search criteria:</u></b><br /><br />
-					<form action="" method=post>
-					<b><label onclick=deinit(1)>Name of the place:</label></b>&nbsp;<input type=text name="place" id=1 value="None"/><br /><br />
-					<b><label onclick=deinit(2)>Type of the place:</label></b>&nbsp;<input type=text name="typeplace" id=2 value="None"/><br /><br />
-					<b><label onclick=deinit(3)>Type of the hotel:</label></b>&nbsp;<input type=text name="typehotel" id=3 value="None"/><br /><br />
-					<b><label onclick=deinit(4)>Budget of the hotel::</label></b><br />
-					<div style="margin-left:50px">
-					Budget Range:&nbsp;lower&nbsp;<input id=41 type=text name="lowerbudget" value="None" />&nbsp;upper&nbsp;<input id=43 type=text name="upperbudget" value="None" /><br />
-					Budget greater than:&nbsp;<input id=44 type=text name="budgetgt" value="None" /><br />
-					Budget lesser than:&nbsp;<input id=45 type=text name="budgetlt" value="None" /><br /><br />
-					</div>
-					<b><label onclick=deinit(5)>Weather of the place:</label></b>&nbsp;<input id=5 type=text name="weather" value="None" /><br /><br />
-					<b><label onclick=deinit(6)>Name of the hotel:</label></b>&nbsp;<input id=6 type=text name="hotel" value="None" /><br /><br />
-					<b><label onclick=deinit(7)>Distance(Both Car & Flight)::</label></b><br />
-					<div style="margin-left:50px">
-					Distance Range:&nbsp;lower&nbsp;<input id=71 type=text name="lowerdistance" value="None"/>&nbsp;upper&nbsp;<input id=73 type=text name="higherdistance" value="None" /><br />
-					Distance greater than:&nbsp;<input id=74 type=text name="distancegt" value="None" /><br />
-					Distance lesser than:&nbsp;<input id=75 type=text name="distancelt" value="None" /><br /><br />
-					</div>
-					<b><label onclick=deinit(8)>Distance(Flight)::</label></b><br />
-					<div style="margin-left:50px">
-					Distance Range:&nbsp;lower&nbsp;<input id=81 type=text name=flowerdistance value="None"/>&nbsp;upper&nbsp;<input id=83 type=text name=fupperdistance value="None" /><br />
-					Distance greater than:&nbsp;<input id=84 type=text name=fdistancegt value="None" /><br />
-					Distance lesser than:&nbsp;<input id=85 type=text name=fdistancelt value="None" /><br /><br />
-					</div>
-					<b><label onclick=deinit(9)>Distance(car)::</label></b><br />
-					<div style="margin-left:50px">
-					Distance Range:&nbsp;lower&nbsp;<input id=91 type=text name=clowerdistance value="None"/>&nbsp;upper&nbsp;<input id=93 type=text name=cupperdistance value="None" /><br />
-					Distance greater than:&nbsp;<input id=94 type=text name=cdistancegt value=None /><br />
-					Distance lesser than:&nbsp;<input id=95 type=text name=cdistancelt value=None /><br /><br />
-					</div>
-					<b><label onclick=deinit(10)>No of days:</label></b>&nbsp;<input id=10 type=text name=days value="None" /><br /><br />
-					<b><label onclick=deinit(11)>City:</label></b>&nbsp;<input id=11 type=text name=city value="None" /><br /><br />
-					<b><label onclick=deinit(12)>State:</label></b>&nbsp;<input id=12 type=text name=state value="None" /><br /><br />
-					<b><label onclick=deinit(13)>Name of the Month:</label></b>&nbsp;<input id=13 type=text name=month value="None" /><br />
-					<br /><center><input type=submit name=submit value=Search style="width:200px"/></center><br />
-					</form></div>
-					</body></html> '''
-		self.wfile.write(form)
-		return
+		form='''<html>
+<head>
+<script>
+function budget(id)
+{
+bud=document.getElementById("bu")
+if(id==3||id==4) {
+bud.style.display="block";
+} 
+else {
+bud.style.display="none";
+} }
+</script>
+</head>
+<body>
+<h1>Query</h1>
+<hr><hr>
+<h3>Type of Tourist Place:</h3>
 
+<form action="" method=post>
+<select size=1 name="Type">
+<option value="none">Select the type of place</option>
+<option id=1  value="Beach" onclick=budget(id)>Beach</option>
+<option id=2  value="Hill Station" onclick=budget(id)>Hill Station</option>
+<option id=3  value="Hotel" onclick=budget(id)>Hotel</option>
+<option id=4  value="Beach Resort" onclick=budget(id)>Beach Resort</option>
+</select>
+<div id=bu style="display:none"><h3>Enter your Budget:</h3>
+<input type=text name="Budget" value="none"/></div>
+<h3>Enter State:</h3>
+<input type=text name="State" value="none"/><br /><br />
+<input type=submit value=Search /><input type=reset />
+</form>
+</body></html>'''
+		
+		self.wfile.write((form))
+class ThreadedHTTPServer(ThreadingMixIn, HTTPServer):
+	"""Handle requests in a separate thread."""
 try:
 	#Create a web server and define the handler to manage the
 	#incoming request
-	server = HTTPServer(('', PORT_NUMBER), myHandler)
+	server = ThreadedHTTPServer(('', PORT_NUMBER), myHandler)
 	print 'Started httpserver on port ' , PORT_NUMBER
 	f = open('log.txt','a')
 	f.write('Started httpserver on port'+str(PORT_NUMBER)+'\n')
--- Unit3/src/mongointerface.py
+++ Unit3/src/mongointerface.py
@@ -1,41 +1,119 @@
-import pymongo
+#import pymongo
 import datetime
 import re
 import linecache
-client = pymongo.Connection('localhost', 27017)
-db = client.tourism_database
-database = db.database
-w= open('input2.txt','r')
+#client = pymongo.Connection('localhost',27017)
+#db = client.tourism_database
+#database = db.database
+w=open('input.txt','r')
+f=open('inputdistance.txt','r')
 i=0
-hill=[]
-tourist=[]
-beach=[]
-hotels=[]
+State=''
+Budget=''
+Type=''
+Name=''
+Preferredby=''
+Flights=''
+Taxi=''
+Trains=''
+Distance=''
+Bus=''
+t=0
 for line in w.readlines():
-    i=i+1
-    if line=="Tourist places\n":
-        input=linecache.getline('input2.txt',i+1).strip('0123456789.\n').split("-")     
-        tourist.append(input[0])
-    elif line=="Hill stations\n":
-        state=linecache.getline('input2.txt',i+1).replace(":-",",").strip('.\n')
-        hill.append(state.split(","))
-    elif line=="Beach\n":
-        beaches=linecache.getline('input2.txt',i+1).strip('.\n')
-        beach.append(beaches)
-    elif line=="hotels\n":
-        hotel=linecache.getline('input2.txt',i+1).strip('.\n').split("-")
-        hotels.append(hotel)
-for j in range(len(hotels)):
-    post={'Type':'Hotels','Place':hotels[j][0],'Name':hotels[j][1]}
-    database.insert(post)
-for j in range(len(hill)):
-    for x in range(1,len(hill[j])):
-        post={'Type':'HillStation','State':hill[j][0],'Name':hill[j][x]}
-        database.insert(post)
-for j in range(len(tourist)):	
-	post={'Type':'Tourist','Name':tourist[j]}
-	database.insert(post)
-for j in range(len(beach)):	
-	post={'Type':'Beaches','Name':beach[j]}
-	database.insert(post)
-
+	i=i+1
+	if line=="Beaches\n":
+		t=1
+		Type='Beach'
+	elif line=="Hill Stations\n":
+		t=2
+		Type='Hill Stations'
+	elif line=="Beach Resorts\n":
+		t=3
+		Type='Beach Resorts'
+	elif line=="Hotels\n":
+		t=4
+		Type='Hotels'
+	elif line=="Heritage Site\n":
+		Type='Heritage Site'
+		t=5
+	if t==3:
+		if re.match('State=',line,re.I):
+			line=line.replace('State=','')
+			State=line.strip('0123456789\n')
+		elif re.match('Name=',line,re.I):
+			Name=line.replace('Name=','').strip('\n')
+			Budget='INR '+linecache.getline('input.txt',i+2).replace('Budget=','').strip('\n')
+			post={'Type':Type,'State':State,'Name':Name,'Budget':Budget}
+			#database.insert(post)
+			#print Type,State,Name,Budget
+	elif t==4:
+		if re.match('State=',line,re.I):
+			line=line.replace('State=','')
+			State=line.strip('0123456789\n')
+		elif re.match('Name=',line,re.I):
+			Name=line.replace('Name=','').strip('\n')
+			Budget='INR '+linecache.getline('input.txt',i+2).replace('Budget=','').strip('\n')
+			post={'Type':Type,'State':State,'Name':Name,'Budget':Budget}
+			#database.insert(post)
+			#print Type,State,Name,Budget
+	elif t==2:
+		if re.match('State=',line,re.I):
+			State=line.replace('State=','').strip('\n')
+		elif re.match('Name=',line,re.I):
+			Name=line.replace('Name=','').strip('\n')
+		elif re.match('Preferred by=',line,re.I):
+			Preferredby=line.replace('Preferred by=','').strip('\n')
+			post={'Type':Type,'State':State,'Name':Name,'Preferred by':Preferredby}
+			#database.insert(post)
+	elif t==1:
+		if re.match('State=',line,re.I):
+			State=line.replace('State=','').strip('\n')
+		elif re.match('Name=',line,re.I):
+			Name=line.replace('Name=','').strip('\n')
+		elif re.match('Preferred by=',line,re.I):
+			Preferredby=line.replace('Preferred by=','').strip('\n')
+			post={'Type':Type,'State':State,'Name':Name,'Preferred by':Preferredby}
+			#database.insert(post)
+	elif t==5:
+		if re.match('State=',line,re.I):
+			State=line.replace('State=','').strip('\n')
+		elif re.match('Name=',line,re.I):
+			Name=line.replace('Name=','').strip('\n')
+		elif re.match('Preferred by=',line,re.I):
+			Preferredby=line.replace('Preferred by=','').strip('\n')
+			post={'Type':Type,'State':State,'Name':Name,'Preferred by':Preferredby}
+			#database.insert(post)
+i=0
+d=0
+for line in f.readlines():
+	i=i+1
+	if re.match('Name=',line,re.I):
+		Name=line.replace("Name=",'').strip('\n')
+		d=d+1
+		#print d
+	elif re.match('Flights',line,re.I):
+		Flights=linecache.getline('inputdistance.txt',i+1).strip('\n')
+		d=d+1
+		#print d
+	elif re.match('Taxi',line,re.I):
+		Taxi=linecache.getline('inputdistance.txt',i+1).strip('\n')
+		d=d+1
+		#print d
+	elif re.match('Bus',line,re.I):
+		Bus=linecache.getline('inputdistance.txt',i+1).strip('\n')
+		d=d+1
+		#print d
+	elif re.match('Trains',line,re.I):
+		Trains=linecache.getline('inputdistance.txt',i+1).strip('\n')	
+		#print Trains
+		d=d+1
+		#print "d=",d
+	elif re.search('Total distance:',line,re.I):
+		line=line.split('km')[0]
+		Distance=line.split('Total')[1].replace('distance','')
+		d=d+1
+		#print d
+	if d==9:
+		d=0
+		post={'Name':Name,'Distance':Distance,'Flight':Flights,'Taxi':Taxi,'Bus':Bus,'Trains':Trains}
+		print post
